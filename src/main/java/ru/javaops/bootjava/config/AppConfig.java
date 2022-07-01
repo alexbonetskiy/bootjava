07da781626f6e04f4da995c9485aa3895df77629
Alexander <ive30@rambler.ru>
2022-07-01 17:56:52 +0300

    update to spring boot 2.7

--- src/main/java/ru/javaops/bootjava/config/AppConfig.java
+++ src/main/java/ru/javaops/bootjava/config/AppConfig.java
@@ -1,13 +1,16 @@
 package ru.javaops.bootjava.config;
 
 import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import lombok.extern.slf4j.Slf4j;
 import org.h2.tools.Server;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import ru.javaops.bootjava.util.JsonUtil;
 
 import java.sql.SQLException;
 
@@ -28,5 +31,10 @@ public class AppConfig {
     @Bean
     Module module() {
         return new Hibernate5Module();
+    }
+
+    @Autowired
+    private void storeObjectMapper(ObjectMapper objectMapper) {
+        JsonUtil.setMapper(objectMapper);
     }
 }
